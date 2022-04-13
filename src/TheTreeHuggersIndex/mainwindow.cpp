5b26773e0bf0eecff27e589eabbdd392c07eae30
thomas914 <thomasli914@gmail.com>
2022-04-13 23:57:22 +0800

    Completion

--- src/TheTreeHuggersIndex/mainwindow.cpp
+++ src/TheTreeHuggersIndex/mainwindow.cpp
@@ -3,7 +3,8 @@
 #include <QPainter>
 #include <QPixmap>
 #include <QDebug>
-#include <QRegularExpressionValidator>
+#include "resultdialog.h"
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -14,6 +15,10 @@ MainWindow::MainWindow(QWidget *parent)
     //pre-load the bg_pic in the constructor
     bg_pic = QPixmap(":/cover/res/cover_pic.jpg");
     showBackground();
+
+    resultDialog = new ResultDialog(this);
+
+    connect(this, &MainWindow::showResult, resultDialog, &ResultDialog::showResult);
 }
 
 MainWindow::~MainWindow()
@@ -87,3 +92,39 @@ void MainWindow::showBackground(bool isShow)
 
     update();
 }
+
+double MainWindow::leakyRelu(double x)
+{
+    if(x > 0)	return x;
+    return 0.0001 * x;
+}
+
+double MainWindow::getESGScore(double x1, double x2, double x3, double x4)
+{
+    x1 = (x1 - 163460.941) / 379449.442;
+    x2 = (x2 - 2795.14804) / 5500.91802;
+    x3 = (x3 - 8.58921569) / 55.0834142;
+    x4 = (x4 - 0.396582822) / 0.262649178;
+
+    double n11 = leakyRelu(1.0135033*x1 + 1.2345738*x2 - 6.452141*x3 + 4.5648273*x4 - 5.4859514);
+    double n12 = leakyRelu(0.31725503*x1 + 2.1164956*x2 - 14.882733*x3 + 9.181947*x4 - 12.591677);
+    double n13 = leakyRelu(0.847759*x1 + 0.8036155*x2 - 5.4198184 *x3 + 3.3207757*x4 - 4.5928507);
+
+    double n21 = leakyRelu(-0.2458616*n11 - 0.03026359*n12 + 0.11779974*n13 - 0.36948147);
+    double n22 = leakyRelu(-15.652485*n11 - 20.366306*n12 - 3.7568526 *n13 - 2.2243917);
+    double n23 = leakyRelu(-14.719395*n11 - 18.694027 *n12 - 3.8506098*n13 - 2.2988038);
+
+    double result = -0.5320807 * n21 - 20.618141 * n22 - 20.632715 * n23 + 24.21233;
+    return result;
+}
+
+void MainWindow::on_pb_create_clicked()
+{
+    double x1 = ui->dsb_numOfEmp->value();
+    double x2 = ui->dsb_marketValue->value();
+    double x3 = ui->dsb_peRatio->value();
+    double x4 = ui->dsb_ranking->value();
+
+    emit showResult(getESGScore(x1, x2, x3, x4));
+}
+
